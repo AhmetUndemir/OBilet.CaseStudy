84a29ccfe945911d06fd01d798af4c3eed19a45e
Ahmet Undemir <ahmet.undemir@infoline-tr.com>
2023-03-11 03:19:27 +0300

    BusLocation manager oluşturuldu

--- OBilet.Business/Conctrete/BusLocationManager.cs
+++ OBilet.Business/Conctrete/BusLocationManager.cs
@@ -2,6 +2,7 @@
 using OBilet.BusinessData;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,20 @@ namespace OBilet.Business
 {
 	public class BusLocationManager : IBusLocationService
 	{
+		public RequestManager _requestManager;
+		public BusLocationManager()
+		{
+			var baseURL = new BaseUrl
+			{
+				Auth = ConfigurationManager.AppSettings["APIKeyInfo"],
+				Url = ConfigurationManager.AppSettings["APIURL"]
+			};
+			_requestManager = new RequestManager(baseURL);
+		}
 		public BusLocationResponseModel GetBusLocations(BusLocation data)
 		{
-			throw new NotImplementedException();
+			//Doldurulacak
+			return _requestManager.Post<BusLocationResponseModel>(data, "location/getbuslocations");
 		}
 	}
 }
